b10edba2a58f076b1dfee19ed729730e9bc222f9
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-02 21:45:58 +0530

    Variables

---
+++ 4.1_Variables.py
@@ -0,0 +1,47 @@
+x = str(3)
+y = int(3)
+z = float(3)
+
+print("Type of x", type(x))
+print("Type of y", type(y))
+print("Type of z", type(z))
+print('------------------------')
+# --------------------------------
+my_variable_name = "Name"
+print("my_variable_name:", my_variable_name)
+print('------------------------')
+# --------------------------------
+x, y, z = "Sun", "Earth", "Moon"
+print(x)
+print(y)
+print(z)
+print('------------------------')
+# --------------------------------
+x = y = z = "Apple"
+print(x)
+print(y)
+print(z)
+print('------------------------')
+# --------------------------------
+
+fruits = ["Apple", "Orange", 'Mango']
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print('------------------------')
+# --------------------------------
+x = "Python is awesome"
+print(x)
+print('------------------------')
+# --------------------------------
+x = 5
+y = 10
+print(x+y)
+print('------------------------')
+# --------------------------------
+x = 5
+y = "John"
+print(x, y)
+print('------------------------')
+# --------------------------------
