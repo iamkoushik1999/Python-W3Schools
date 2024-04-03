d7b0debdaf91b2405f2b519648f2c6cc4d2136f4
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-03 21:47:48 +0530

    5 Data Types

---
+++ 5_Data_type.py
@@ -0,0 +1,44 @@
+a = "Hello World"
+print("a = 'Hello World' :", type(a))
+
+b = 20
+print("b = 20 :", type(b))
+
+c = 20.5
+print("c = 20.5 :", type(c))
+
+d = 1j
+print("d = 1j :", type(d))
+
+e = ["apple", "banana"]
+print("e = ['apple' 'banana'] :", type(e))
+
+f = ("apple", "banana")
+print("f = ('apple', 'banana') :", type(f))
+
+g = range(5)
+print("g = range(5) :", type(g))
+
+h = {"name": "John", "age": 24}
+print("h = {'name': 'John', 'age': 24}:", type(h))
+
+i = {"apple", "banana"}
+print("i = {'apple', 'banana'}:", type(i))
+
+j = frozenset({"apple", "banana"})
+print("j = frozenset({'apple', 'banana'}) :", type(j))
+
+k = True
+print("k = True :", type(k))
+
+m = bytearray(5)
+print("m = bytearray(5) :", type(m))
+
+n = memoryview(bytes(5))
+print("n = memoryview(bytes(5)) :", type(n))
+
+o = None
+print("o = None :", type(o))
+
+p = b"Hello"
+print("p = b'Hello' :", type(p))
