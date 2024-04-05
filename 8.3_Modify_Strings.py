84eb29b6f83accc51cb1ff3ae1c73d2d1f1d23e7
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-05 21:52:22 +0530

    8.3 Modify Strings

---
+++ 8.3_Modify_Strings.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"
+print("Upper")
+print(a.upper())
+print("Lower")
+print(a.lower())
+
+b = " Hello, World! "
+print("Strip")
+print(b.strip())
+
+print("Replace")
+print(a.replace("l", "k"))
+
+print("Split")
+print(a.split(','))
