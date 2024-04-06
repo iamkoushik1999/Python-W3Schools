87b958ea3b7d91196c9490b0044fe0c1fb95481d
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-06 21:55:48 +0530

    10 Operators

---
+++ 10_Operators.py
@@ -0,0 +1,78 @@
+# Arithmatic Operators
+a = 10
+b = 5
+# SUM
+add = a + b
+print(add)
+# SUB
+sub = a - b
+print(sub)
+# MUL
+mul = a * b
+print(mul)
+# DIV
+div = a/b
+print(div)
+# MODULUS
+mod = a % b
+print(mod)
+# EXPONENTIATIONS
+Expo = a ** b
+print(Expo)
+# Floor Division
+floor = a // b
+print(floor)
+print("------------------------------")
+# Assignment Operators
+x = 10
+y = 5
+
+# x += 3
+# print(x)
+# x -= 3
+# print(x)
+# x *= 3
+# print(x)
+# x /= 3
+# print(x)
+# x %= 3
+# print(x)
+# x //= 3
+# print(x)
+# x **= 3
+# print(x)
+# x &= 3
+# print(x)
+# x |= 3
+# print(x)
+# x ^= 3
+# print(x)
+# x >>= 3
+# print(x)
+# x <<= 3
+# print(x)
+print("------------------------------")
+# Comparison Operators
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print("------------------------------")
+# Logical Operators
+print(x < 5 and x < 10)
+print(x < 5 and x < 4)
+print(not (x < 5 and x < 10))
+print("------------------------------")
+# Identify Operators
+p = ["Banana", "Apple"]
+q = ["Banana", "Apple"]
+r = p
+print(p is r)
+print(p is q)
+print(p is not q)
+print("------------------------------")
+# # Membership Operators
+print("Banana" in p)
+print("Mango" not in p)
