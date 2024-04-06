607c9e0f2be71cd8c122af8a0b1fd46c04c99ffb
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-06 21:54:08 +0530

    8.6 Escape Characters

---
+++ 8.6_Escape_Characters.py
@@ -0,0 +1,19 @@
+# Single Quote
+txt1 = "I am a \"Software Developer\" "
+print(txt1)
+
+# Backslash
+txt2 = "I know MERN \\ Backend Developer"
+print(txt2)
+
+# New Line
+txt3 = "I know \nreact \nnode \nexpress \nmongodb"
+print(txt3)
+
+# Tab
+txt4 = "I am learing Python\t\tTypeScript"
+print(txt4)
+
+# Backspace
+txt5 = "I know Java \bScript"
+print(txt5)
