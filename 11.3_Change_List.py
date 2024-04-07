2a4fee13fb153f105970b523ee8f4bf730c221bb
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-07 21:57:27 +0530

    11.3 Change List

---
+++ 11.3_Change_List.py
@@ -0,0 +1,11 @@
+list1 = ['apple', 'banana', 'orange']
+list1[1] = 'kiwi'
+print(list1)
+
+list2 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+list2[1:3] = ["blackcurrant", "watermelon"]
+print(list2)
+
+list3 = ['apple', 'banana', 'orange']
+list3.insert(2,"kiwi")
+print(list3)
