1f3af2ea484ec3e43f83de99401babc598872aec
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-07 21:56:57 +0530

    11.2 Access List

---
+++ 11.2_Access_List.py
@@ -0,0 +1,15 @@
+list1 = ['apple', 'banana', 'mango']
+print(list1[1])
+
+list2 = ['apple', 'orange', 'mango']
+print(list2[-1])
+
+list3 = ['apple', 'banana', 'orange', 'mango', 'kiwi']
+print(list3[2:5])
+
+print(list3[:4])
+
+print(list3[2:])
+
+if 'apple' in list3:
+    print('Yes, apple is there')
