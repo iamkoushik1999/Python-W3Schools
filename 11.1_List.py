be8172164914b2e7dc15347cd1ab88273df0820d
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-07 21:56:23 +0530

    11.1 List

---
+++ 11.1_List.py
@@ -0,0 +1,29 @@
+myList1 = ["apple", "banana", "mango"]
+print(myList1)
+
+# List item scan be ordered, Changeable and Duplicate
+myList2 = ['apple', 'banana', 'mango', 'apple', 'orange']
+print(myList2)
+
+# List length
+print(len(myList1))
+print(len(myList2))
+
+# List
+list1 = ["apple", "banana", "orange"]
+list2 = [1, 4, 8, 10, 4]
+list3 = [True, False]
+print(list1)
+print(list2)
+print(list3)
+
+# List Multiple
+list4 = ['abc', 34, True]
+print(list4)
+
+# Type
+list5 = ['apple', 'banana', 'mango']
+print(type(list5))
+
+#
+
