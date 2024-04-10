32c2b1c8f365fb4afbddccfe3cf8e96db3b5e8b1
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-10 11:26:24 +0530

    11.9 Copy List

---
+++ 11.9_Copy_List.py
@@ -0,0 +1,8 @@
+# Copy List
+myList1 = ['apple', 'banana', 'orange', 'mango']
+newList1 = myList1.copy()
+print(newList1)
+print('------------------------------------------------')
+# Copy List with 'list()' method
+newList2 = list(myList1)
+print(newList2)
