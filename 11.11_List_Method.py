49ffb670a492150c0b02917275120d50ec4baea9
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-10 11:52:38 +0530

    11.11 List Methods

---
+++ 11.11_List_Method.py
@@ -0,0 +1,50 @@
+# append
+myList1 = ['apple', 'banana']
+myList1.append('kiwi')
+print(myList1)
+print('-------------------------------------')
+# clear
+myList1.clear()
+print(myList1)
+print('-------------------------------------')
+# copy
+myList2 = [1, 2, 3, 4]
+myList3 = myList2.copy()
+print(myList2)
+print('-------------------------------------')
+# count
+myList4 = [99, 79, 69, 99, 33]
+count = myList4.count(99)
+print(count)
+print('-------------------------------------')
+# extend
+myList5 = ['cricket', 'football']
+myList6 = ['rugby', 'baseball']
+myList5.extend(myList6)
+print(myList5)
+print('-------------------------------------')
+# index
+myList7 = ['kolkata', 'mumbai', 'delhi', 'punjab']
+index = myList7.index('kolkata')
+print(index)
+print('-------------------------------------')
+# insert
+myList8 = ['apple', 'banana', 'kiwi']
+myList8.insert(1, 'orange')
+print(myList8)
+print('-------------------------------------')
+# pop
+myList8.pop(2)
+print(myList8)
+print('-------------------------------------')
+# pop
+myList8.remove('kiwi')
+print(myList8)
+print('-------------------------------------')
+# reverse
+myList8.reverse()
+print(myList8)
+print('-------------------------------------')
+# sort
+myList7.sort()
+print(myList7)
