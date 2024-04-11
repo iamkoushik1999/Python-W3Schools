aec517ae96a32752bb5a99d103df521e78d8a786
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-11 16:41:46 +0530

    7.7 Tuple Methods

---
+++ 7. Tuples/7.7_Tuple_Method.py
@@ -0,0 +1,9 @@
+# Count
+tuple1 = (1, 2, 5, 4, 5, 6, 5, 8, 9, 5)
+tuple1_Count = tuple1.count(5)
+print(tuple1_Count)
+print('-----------------------------')
+# Index
+tuple1_Index = tuple1.index(5)
+print(tuple1_Index)
+
