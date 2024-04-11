fc5e76b61b15c825244781692ad4e87b2d7e1c6f
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-11 10:56:23 +0530

    Tuple -> 7.1 Tuples

---
+++ 7. Tuples/7.1_Tuples.py
@@ -0,0 +1,20 @@
+myTuple1 = ('apple', 'banana', 'kiwi')
+print(myTuple1)
+# Tuple items are ordered, unchangeable and allow duplicate values
+# Length
+print(len(myTuple1))
+# One Tuple Item
+myTuple2 = ('apple',)
+print(myTuple2)
+print(type(myTuple2))
+# Not a Tuple
+myTuple3 = ('apple')
+print(myTuple3)
+print(type(myTuple3))
+# Tuple Can Contain any data type
+anyTuple = ('apple', 1, True, False)
+print(anyTuple)
+# Tuple Constructor
+# Use tuple() constructor to make a tuple
+thisTuple = tuple(('apple', 'banana', 'cherry'))
+print(thisTuple)
