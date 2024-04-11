6949b712dbf36b513b6b8a345ba71513ce96b970
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-11 15:52:10 +0530

    7.5 Loop Tuples

---
+++ 7. Tuples/7.5_Loop_Tuple.py
@@ -0,0 +1,15 @@
+# For Loop
+basket1 = ('apple', 'banana', 'cherry')
+
+for x in basket1:
+    print(x)
+print('-----------------------------------------')
+# For Loop with Range and Lenth
+for i in range(len(basket1)):
+    print(basket1[i])
+print('-----------------------------------------')
+# While loop
+i = 0
+while i < len(basket1):
+    print(basket1[i])
+    i = i + 1
