349b52c19fa2010bb7d91fbf5b5b1e8df0896408
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-17 11:24:03 +0530

    8.5 Loop Sets

---
+++ 8. Sets/8.5_Loop_Sets.py
@@ -0,0 +1,10 @@
+# Looping with sets
+mySet = {'apple', 'banana', 'cherry', 'mango', 'kiwi', 'orange', 'pinapple'}
+# TO print all items in set
+for x in mySet:
+    print(x)
+print('--------------------------------------------------------------------------------')
+# To print whole set for the items that it has
+for x in mySet:
+    print(mySet)
+
