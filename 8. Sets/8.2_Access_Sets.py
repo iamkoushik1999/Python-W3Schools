7c6e6555b5347911703c36ee1b316879488393b9
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-17 10:52:31 +0530

    8.2 Access Set Items

---
+++ 8. Sets/8.2_Access_Sets.py
@@ -0,0 +1,11 @@
+# Access Set Items
+mySet1 = {'Apple', 'Banana', 'Cherry'}
+for x in mySet1:
+    print(x)
+print('---------------------------------')
+# Check if banana is there
+print("Banana" in mySet1)
+print('---------------------------------')
+print("banana" not in mySet1)
+print('---------------------------------')
+
