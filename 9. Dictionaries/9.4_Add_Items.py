d9721853ee9105fe876404d3f32771b9a873f444
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-18 11:16:54 +0530

    9.4 Add Items

---
+++ 9. Dictionaries/9.4_Add_Items.py
@@ -0,0 +1,17 @@
+phone = {
+    "brand": "Google",
+    "model": "Pixel",
+    "year": 2024
+}
+
+phone['color'] = 'red'
+print(phone)
+
+phone = {
+    "brand": "Apple",
+    "model": "Iphone",
+    "year": 2024
+}
+
+x = phone.update({'color': 'white'})
+print(phone)
