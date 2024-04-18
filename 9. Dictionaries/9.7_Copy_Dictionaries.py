a35e764630e0497201cf51ba6e6dff3c97b6aed9
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-18 12:36:25 +0530

    9.7 Copy_Dictionaries

---
+++ 9. Dictionaries/9.7_Copy_Dictionaries.py
@@ -0,0 +1,20 @@
+phone1 = {
+    'brand': 'Apple',
+    'model': 'Iphone',
+    'year': 2024,
+    'color': 'white'
+}
+
+phone2 = phone1.copy()
+print(phone2)
+print('-----------------------------------------')
+
+car1 = {
+    'brand': 'Ford',
+    'model': 'Mustang',
+    'year': 2024,
+    'color': 'red'
+}
+car2 = dict(car1)
+print(car2)
+
