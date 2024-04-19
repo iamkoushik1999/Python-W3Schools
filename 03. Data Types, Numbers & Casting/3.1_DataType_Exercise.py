1d7c90a6212a9488a2fbdb768b39ab86c1021ad0
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-19 12:14:46 +0530

    3.1_DataType_Exercise.py

---
+++ 03. Data Types, Numbers & Casting/3.1_DataType_Exercise.py
@@ -0,0 +1,42 @@
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 1')
+x = 5
+print(type(x))
+print('int')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 2')
+x = "Hello World"
+print(type(x))
+print('str')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 3')
+x = 20.5
+print(type(x))
+print('float')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 4')
+x = ["apple", "banana", "cherry"]
+print(type(x))
+print('list')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 5')
+x = ("apple", "banana", "cherry")
+print(type(x))
+print('tuple')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 6')
+x = {"name": "John", "age": 36}
+print(type(x))
+print('dict')
+print('----------------------------------------------')
+# The following code example would print the data type of x, what data type would that be?
+print('Exercise 7')
+x = True
+print(type(x))
+print('bool')
+print('----------------------------------------------')
