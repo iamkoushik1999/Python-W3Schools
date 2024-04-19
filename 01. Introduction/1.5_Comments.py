285e112240703a7c3589cf5314eb39ab12b02199
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-19 11:33:36 +0530

    1.5_Comments.py

---
+++ 01. Introduction/1.5_Comments.py
@@ -0,0 +1,32 @@
+# This is a comment
+print("Hello, World!")
+print('--------------------------------------------------')
+print("Hello, World!")  # This is a comment
+print('--------------------------------------------------')
+# print("Hello, World!")
+print("Cheers, Mate!")
+print('--------------------------------------------------')
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+print('--------------------------------------------------')
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+print('--------------------------------------------------')
+# Comments in Python are written with a special character, which one?
+print('Excercise 1')
+# This is a comment
+print('--------------------------------------------------')
+# Use a multiline string to make the a multiline comment:
+print('Excercise 2')
+"""
+This is a comment
+written in 
+more than just one line
+"""
+print('--------------------------------------------------')
