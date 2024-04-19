a5609721e0fa50b06efddb3e3835a3d2188d6dc6
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-19 11:36:29 +0530

    1.4_Syntax.py

--- 01. Introduction/1.4_Syntax.py
+++ 01. Introduction/1.4_Syntax.py
@@ -1,7 +1,17 @@
-if(5 > 2):
+if 5 > 2:
     print("5 is greater than 2")
 
 x = "Hello"
 y = "World"
 
 print(x, ",", y)
+print('--------------------------------------------------')
+# Comments in Python are written with a special character, which one?
+print('Excercise 1')
+print("Hello World")
+print('--------------------------------------------------')
+# Complete the code block, print "YES" if 5 is larger than 2.
+# Hint: remember the indentation.
+if 5 > 2:
+    print("YES")
+    
