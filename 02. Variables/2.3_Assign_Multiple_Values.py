095cbd5ef592dc8ab6f16936c77f93b79a829eb2
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-19 11:45:09 +0530

    2.1_Variables.py 2.3_Assign_Multiple_Values.py| 2.4_Output_Variables.py

---
+++ 02. Variables/2.3_Assign_Multiple_Values.py
@@ -0,0 +1,20 @@
+x, y, z = "Sun", "Earth", "Moon"
+print(x)
+print(y)
+print(z)
+print('------------------------')
+# --------------------------------
+x = y = z = "Apple"
+print(x)
+print(y)
+print(z)
+print('------------------------')
+# --------------------------------
+
+fruits = ["Apple", "Orange", 'Mango']
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print('------------------------')
+
