7c562c36a8169951c1ac5a324c5b861af0541b92
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-04-19 12:21:33 +0530

    3.2_Numbers_Exercise.py

---
+++ 03. Data Types, Numbers & Casting/3.2_Numbers_Exercise.py
@@ -0,0 +1,18 @@
+# Insert the correct syntax to convert x into a floating point number.
+print('Exercise 1')
+x = 5
+x = float(x)
+print(x)
+print('-------------------------------')
+# Insert the correct syntax to convert x into integer.
+print('Exercise 2')
+x = 5.5
+x = int(x)
+print(x)
+print('-------------------------------')
+# Insert the correct syntax to convert x into a complex number.
+print('Exercise 3')
+x = 5
+x = complex(x)
+print(x)
+print('-------------------------------')
